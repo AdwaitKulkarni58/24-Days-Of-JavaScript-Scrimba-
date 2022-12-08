a2e7dfc6746e4030946bc9071d032e5520e3b73b
Adwait Kulkarni <adwait.kul.2018@gmail.com>
2022-12-07 16:40:11 -0800

    Create day 8 solution

---
+++ Day-8/Solution.js
@@ -0,0 +1,54 @@
+function validTime(str) {
+    //  write code here.
+    let [hours, mins] = str.split(":");
+    return hours <= 24 && hours >= 0 && parseInt(mins) <= 60 && parseInt(mins) >= 0;
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('validTime()', () => {
+    it('returns true for valid time', () => {
+        // arrange
+        const str = '13:58';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(true);
+    });
+
+    it('returns false when invalid hours', () => {
+        // arrange
+        const str = '25:51';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+
+    it('returns false when invalid minutes', () => {
+        // arrange
+        const str = '02:76';
+        
+        // act
+        const result = validTime(str);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(false);
+    });
+});
